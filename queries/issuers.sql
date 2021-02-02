381ec4eda1b4e11735250f48e00589ad17d60a30
Almir Sarajčić <almir.sarajcic@icloud.com>
2021-02-02 19:21:15 +0100

    Calculate previous dividends

--- queries/issuers.sql
+++ queries/issuers.sql
@@ -8,8 +8,11 @@ SELECT
   total_shares,
   nominal_price,
   total_dividends::DECIMAL,
+  previous_total_dividends::DECIMAL,
   ROUND(total_dividends::DECIMAL / total_shares, 2) AS dividend,
-  (CASE WHEN (info->'LastTradePrice')::FLOAT = 0 THEN NULL ELSE ROUND((total_dividends::DECIMAL / total_shares) / (info->'LastTradePrice')::DECIMAL * 100, 2) END) AS dividend_roi
+  ROUND(previous_total_dividends::DECIMAL / total_shares, 2) AS previous_dividend,
+  (CASE WHEN (info->'LastTradePrice')::FLOAT = 0 THEN NULL ELSE ROUND((total_dividends::DECIMAL / total_shares) / (info->'LastTradePrice')::DECIMAL * 100, 2) END) AS dividend_roi,
+  (CASE WHEN (info->'LastTradePrice')::FLOAT = 0 THEN NULL ELSE ROUND((previous_total_dividends::DECIMAL / total_shares) / (info->'LastTradePrice')::DECIMAL * 100, 2) END) AS previous_dividend_roi
 FROM issuers
 JOIN (
   SELECT issuer_id, total_shares, nominal_price
@@ -35,4 +38,15 @@ LEFT JOIN (
     JSONB_ARRAY_ELEMENTS(financial_statements.statement->'EQUITYCHANGES') equity_changes
   WHERE equity_changes->>'Description' = '21. Objavljene dividende i drugi oblici raspodjele dobiti i pokriće gubitka'
 ) equity_changes ON equity_changes.issuer_id = issuers.id
+LEFT JOIN (
+  SELECT issuer_id, equity_changes->>'TotalCapital' AS previous_total_dividends
+  FROM
+    (
+      SELECT DISTINCT ON (issuer_id, year, semi_annual) issuer_id, statement
+      FROM financial_statements
+      WHERE year = 2018 AND semi_annual = FALSE
+    ) financial_statements,
+    JSONB_ARRAY_ELEMENTS(financial_statements.statement->'EQUITYCHANGES') equity_changes
+  WHERE equity_changes->>'Description' = '21. Objavljene dividende i drugi oblici raspodjele dobiti i pokriće gubitka'
+) previous_equity_changes ON previous_equity_changes.issuer_id = issuers.id
 ORDER BY dividend_roi DESC NULLS LAST, last_date DESC
