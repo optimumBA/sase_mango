bdb026b0d8de15755c483e89a121092fe59b1b52
Almir Sarajčić <almir.sarajcic@icloud.com>
2021-11-22 19:52:08 +0100

    Fix finding message textarea

--- config/config.exs
+++ config/config.exs
@@ -40,6 +40,8 @@ config :sase_mango, SaseMango.Scheduler,
   ],
   timezone: "Europe/Sarajevo"
 
+config :wallaby, max_wait_time: 10_000
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
