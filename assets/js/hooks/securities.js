f17fe50c1cfb59a09f5e3638b35259d693ab94ac
Almir Sarajčić <almir.sarajcic@icloud.com>
2021-12-11 11:10:13 +0100

    Remove annoying sounds

--- assets/js/hooks/securities.js
+++ assets/js/hooks/securities.js
@@ -1,23 +1,11 @@
 let SecuritiesHooks = {}
 
-const soundSrcs = [
-  'audio/bell.wav',
-  'audio/cat_meow.wav',
-  'audio/cat_meow2.wav',
-  'audio/tiger_growl.wav',
-]
-
 SecuritiesHooks.SecuritiesList = {
   mounted() {
     let hook = this
 
     this.requestNotificationPermission(() => {
       hook.handleEvent('send_notification', ({ body }) => {
-        let randomIndex = Math.floor(Math.random() * soundSrcs.length)
-        let soundSrc = soundSrcs[randomIndex]
-        let notificationSound = new Audio(soundSrc)
-        notificationSound.play()
-
         new Notification('SaseMango', { body: body })
       })
     })
