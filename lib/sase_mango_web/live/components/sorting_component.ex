238406705f6e77bc9c3ef9797cfa31c248603419
Daniel Molnar <daniel4molnar@gmail.com>
2022-07-26 15:07:10 +0200

    Convert SortingComponent to function component

--- lib/sase_mango_web/live/components/sorting_component.ex
+++ lib/sase_mango_web/live/components/sorting_component.ex
@@ -1,65 +1,47 @@
 defmodule SaseMangoWeb.Components.SortingComponent do
-  use SaseMangoWeb, :live_component
+  use Phoenix.Component
 
   alias SaseMangoWeb.Components.TableIconsComponent
 
-  def render(assigns) do
+  def sort_link(assigns) do
     ~H"""
       <div
         phx-click="sort_column"
-        phx-target={@myself}
-        class="sortable_column"
+        phx-value-key={@key}
+        class="sortable-column"
       >
         <span><%= @col_text %></span>
 
-        <%= if @col_type == :text && @sort_options.sort_by == @key do %>
-          <div class={"#{icon_color(@sort_options, @key)}"}>
-            <%= if @sort_options.sort_order == :asc do %>
-              <TableIconsComponent.text_sort_asc />
-            <% else %>
-              <TableIconsComponent.text_sort_desc />
-            <% end %>
-          </div>
-        <% end %>
-
-        <%= if @col_type == :text && @sort_options.sort_by != @key do %>
-          <div>
-            <TableIconsComponent.text_sort_asc />
-          </div>
-        <% end %>
-
-        <%= if @col_type == :number do %>
-          <div class={"#{icon_color(@sort_options, @key)}
-                      #{icon_direction(@sort_options, @key)}"}>
-            <TableIconsComponent.number_sort_icon />
-          </div>
-        <% end %>
+        <div class={"
+            #{get_icon_color(@sort_options, @key)}
+            #{get_icon_direction(@col_type, @sort_options, @key)}
+          "}>
+          <TableIconsComponent.sort_icon
+            col_type={@col_type}
+            sort_options={@sort_options.sort_order}
+            key={@key}
+           />
+        </div>
       </div>
     """
   end
 
-  def handle_event("sort_column", _params, socket) do
-    %{sort_options: %{sort_order: sort_order}, key: key} = socket.assigns
-
-    sort_order = if sort_order == :asc, do: :desc, else: :asc
-    sort_options = %{sort_by: key, sort_order: sort_order}
-
-    send(self(), {:url_update, sort_options})
-
-    {:noreply, assign(socket, sort_options: sort_options)}
+  defp get_icon_color(%{sort_by: sort_key} = sort_options, key) when sort_key == key do
+    icon_color(sort_options.sort_order)
   end
 
-  defp icon_color(sort_options, key) do
-    if sort_options.sort_by == key do
-      if sort_options.sort_order == :desc, do: "text-[#FF1010]", else: "text-[#15FF10]"
-    else
-      "text-white"
-    end
+  defp get_icon_color(_sort_options, _key), do: nil
+
+  defp icon_color(:asc), do: "text-[#15FF10]"
+  defp icon_color(:desc), do: "text-[#FF1010]"
+
+  defp get_icon_direction(:number, %{sort_by: sort_key} = sort_options, key)
+       when sort_key == key do
+    get_icon_rotation(sort_options.sort_order)
   end
 
-  defp icon_direction(sort_options, key) do
-    if sort_options.sort_by == key do
-      if sort_options.sort_order == :asc, do: "rotate-down", else: "rotate-up"
-    end
-  end
+  defp get_icon_direction(_type, _sort_options, _key), do: nil
+
+  defp get_icon_rotation(:asc), do: "rotate-down"
+  defp get_icon_rotation(:desc), do: "rotate-up"
 end
