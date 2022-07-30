689acaa66e067e086a21cffc5199531bf98fb597
Daniel Molnar <daniel4molnar@gmail.com>
2022-07-30 16:50:51 +0200

    Create custom select component

--- lib/sase_mango/calculator.ex
+++ lib/sase_mango/calculator.ex
@@ -8,13 +8,14 @@ defmodule SaseMango.Calculator do
   def run([], _input), do: []
 
   def run(securities, %Input{} = input) do
-    security = Enum.find(securities, fn %{symbol: symbol} -> symbol == input.symbol end)
+    security = Enum.find(securities, &(&1.symbol == input.symbol))
 
     total_without_fee = Decimal.mult(input.amount, input.price)
     fee_part = Decimal.div(Decimal.mult(total_without_fee, input.fee), 100)
     total_with_fee = Decimal.add(total_without_fee, fee_part)
 
     %{
+      id: security.symbol <> Decimal.to_string(input.amount) <> Decimal.to_string(input.price),
       name: security.name,
       amount: input.amount,
       symbol: security.symbol,
@@ -22,5 +23,29 @@ defmodule SaseMango.Calculator do
       total_without_fee: total_without_fee,
       total_with_fee: total_with_fee
     }
+  end
+
+  def add_new_item(results, result) do
+    total_without_fee = Decimal.add(results.total_without_fee, result.total_without_fee)
+    total_with_fee = Decimal.add(results.total_with_fee, result.total_with_fee)
+
+    %{
+      securities: [result | results.securities],
+      total_without_fee: total_without_fee,
+      total_with_fee: total_with_fee
+    }
+  end
+
+  def maybe_add_item(securities, results, input) when length(results.securities) > 0 do
+    result = run(securities, input)
+
+    case Enum.any?(results.securities, &(&1.id == result.id)) do
+      true -> results
+      false -> add_new_item(results, result)
+    end
+  end
+
+  def maybe_add_item(securities, results, input) do
+    add_new_item(results, run(securities, input))
   end
 end
--- lib/sase_mango_web/live/calculator_live/index.ex
+++ lib/sase_mango_web/live/calculator_live/index.ex
@@ -4,6 +4,7 @@ defmodule SaseMangoWeb.CalculatorLive.Index do
   alias Phoenix.Socket.Broadcast
   alias SaseMango.Calculator
   alias SaseMango.Securities
+  alias SaseMangoWeb.Components.CustomSelectComponent
   alias SaseMangoWeb.Components.HeaderComponent
   alias SaseMangoWeb.Components.TableIconsComponent
   alias SaseMangoWeb.Endpoint
@@ -16,7 +17,9 @@ defmodule SaseMangoWeb.CalculatorLive.Index do
       |> assign_results()
       |> assign(:securities, Securities.list_securities(:bargains))
       |> assign(:active_tab, :calculator)
-      |> get_symbols_list()
+      |> assign(:select_open, false)
+      |> assign(:select_item, %{key: "", value: nil})
+      |> get_select_list()
 
     if connected?(socket) do
       Endpoint.subscribe("securities")
@@ -37,12 +40,36 @@ defmodule SaseMangoWeb.CalculatorLive.Index do
 
   @impl true
   def handle_event("validate", %{"input" => input_params}, socket) do
+    select_item = socket.assigns.select_item
+
+    changeset = Calculator.change_input(%Calculator.Input{}, input_params)
+
+    changes = Map.merge(changeset.changes, %{symbol: select_item.key})
+
     changeset =
-      %Calculator.Input{}
-      |> Calculator.change_input(input_params)
+      changeset
+      |> Map.put(:changes, changes)
       |> Map.put(:action, :validate)
 
+    IO.inspect(changeset)
     {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("toggle", _params, socket) do
+    {:noreply, assign(socket, :select_open, !socket.assigns.select_open)}
+  end
+
+  def handle_event("hide_select", _params, socket) do
+    {:noreply, assign(socket, :select_open, false)}
+  end
+
+  def handle_event("custom_select", %{"symbol" => symbol} = _params, socket) do
+    selected_item = Enum.find(socket.assigns.select_list, &(&1.key == symbol))
+
+    {:noreply,
+     socket
+     |> assign(:select_open, false)
+     |> assign(:select_item, selected_item)}
   end
 
   def handle_event("calculate", %{"input" => input_params}, socket) do
@@ -60,23 +87,14 @@ defmodule SaseMangoWeb.CalculatorLive.Index do
   def handle_event("delete_row", %{"row_id" => row_id} = _params, socket) do
     socket_results = socket.assigns.results
 
-    target_row =
-      Enum.find(
-        socket_results.securities,
-        fn %{id: id} -> id == row_id end
-      )
-
-    new_rows =
-      Enum.filter(
-        socket_results.securities,
-        fn %{id: id} -> id != row_id end
-      )
+    target_row = Enum.find(socket_results.securities, &(&1.id == row_id))
+    new_rows = Enum.filter(socket_results.securities, &(&1.id != row_id))
 
     new_results = %{
       securities: new_rows,
       total_without_fee:
         Decimal.sub(socket_results.total_without_fee, target_row.total_without_fee),
-      total_with_fee: Decimal.sub(socket_results.total_without_fee, target_row.total_with_fee)
+      total_with_fee: Decimal.sub(socket_results.total_with_fee, target_row.total_with_fee)
     }
 
     {:noreply, assign(socket, :results, new_results)}
@@ -92,45 +110,31 @@ defmodule SaseMangoWeb.CalculatorLive.Index do
      |> calculate()}
   end
 
-  def get_symbols_list(socket) do
+  def get_select_list(socket) do
     securities = socket.assigns.securities
 
-    symbols_list =
-      Enum.reduce(securities, [], fn %{symbol: symbol, name: name} = _security, symbol_list ->
-        [[key: symbol <> "-" <> name, value: symbol] | symbol_list]
+    select_list =
+      Enum.reduce(securities, [], fn %{symbol: symbol, name: name} = _security, select_list ->
+        [%{key: symbol, value: name} | select_list]
       end)
 
-    assign(socket, :symbols_list, symbols_list)
+    assign(socket, :select_list, select_list)
   end
 
   def enable_form_submit?(changeset), do: changeset.valid?
+  def enable_forms_fields?(select_item) when is_binary(select_item.key), do: String.length(select_item.key) > 0
+  def enable_forms_fields?(_select_item), do: false
 
   defp calculate(socket) do
+    socket_results = socket.assigns.results
+
     case Ecto.Changeset.apply_action(socket.assigns.changeset, :insert) do
       {:ok, input} ->
-        result = Calculator.run(socket.assigns.securities, input)
-
-        total_without_fee =
-          Decimal.add(socket.assigns.results.total_without_fee, result.total_without_fee)
-
-        total_with_fee = Decimal.add(socket.assigns.results.total_with_fee, result.total_with_fee)
-
-        new_security =
-          Map.put(
-            result,
-            :id,
-            result.symbol <> Decimal.to_string(result.amount) <> Decimal.to_string(result.price)
-          )
-
-        new_results = %{
-          securities: [new_security | socket.assigns.results.securities],
-          total_without_fee: total_without_fee,
-          total_with_fee: total_with_fee
-        }
-
-        IO.inspect(new_results)
-
-        assign(socket, :results, new_results)
+        assign(
+          socket,
+          :results,
+          Calculator.maybe_add_item(socket.assigns.securities, socket_results, input)
+        )
 
       {:error, changeset} ->
         assign(socket, :changeset, changeset)
