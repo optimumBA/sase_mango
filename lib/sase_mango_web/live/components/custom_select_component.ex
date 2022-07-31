600565564b206d7d32c4d73ecfff842ba3532148
Daniel Molnar <daniel4molnar@gmail.com>
2022-07-31 22:14:54 +0200

    Minor style fixes

--- lib/sase_mango_web/live/components/custom_select_component.ex
+++ lib/sase_mango_web/live/components/custom_select_component.ex
@@ -15,20 +15,20 @@ defmodule SaseMangoWeb.Components.CustomSelectComponent do
     >
       <div class="relative mt-1">
         <div
-          class="relative w-full px-[1rem] py-[0.9rem] text-left bg-white border border-gray-500 cursor-default rounded-[0.6rem] shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 text-[1em] font-[300]"
+          class="relative cursor-pointer w-full px-[1rem] py-[0.9rem] text-left bg-white border border-[#979797] hover:border-[#565555] cursor-default rounded-[0.6rem] shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 text-[1em] font-[300]"
           x-ref="button"
           phx-click="toggle"
           phx-click-away="hide_select"
         >
-          <span class="flex text-left items-center gap-2 mr-16 cursor-pointer">
+          <span class="flex text-left items-center gap-2 mr-16">
           <%= if String.length(@selected_item.key) > 0 do %>
             <span class="block truncate text-[#5B92D7]"><%= @selected_item.key %></span>
             <span class="block truncate text-[#1E1E1E]"><%= @selected_item.value %></span>
           <% else %>
-            <span class="block truncate text-[#B5B5B5] pr-8">SELECT</span>
+            <span class="block truncate text-[#a3a3a3] pr-8">SELECT</span>
           <% end %>
           </span>
-          <span class="absolute cursor-pointer inset-y-0 right-0 flex items-center pr-4 ml-3">
+          <span class="absolute inset-y-0 right-0 flex items-center pr-4 ml-3">
             <svg width="15" height="8"
               viewBox="0 0 15 9"
               fill="none"
@@ -41,12 +41,12 @@ defmodule SaseMangoWeb.Components.CustomSelectComponent do
 
         <%= if @open do %>
           <ul
-              class={"absolute z-10 #{if String.length(@selected_item.key) > 0, do: "w-full", else: "w-max" } py-1 mt-1 overflow-y-auto text-[1em] font-[300] bg-white shadow-lg max-h-[30rem] rounded-lg ring-1 ring-black ring-opacity-5 focus:outline-none"}
+              class={"absolute z-10 #{if String.length(@selected_item.key) > 0, do: "w-full", else: "w-max" } py-1 mt-1 overflow-y-auto text-[1em] font-[300] bg-white drop-shadow-lg max-h-[30rem] rounded-lg ring-2 ring-black ring-opacity-5 focus:outline-none"}
               role="listbox"
             >
             <%= for option <- @options do %>
               <li
-                class="relative cursor-default select-none"
+                class="relative select-none"
                 role="option"
               >
                 <div class="flex flex-col gap-2 py-2 pl-3 pr-9 border-b border-gray-100 cursor-pointer hover:bg-sky-100"
