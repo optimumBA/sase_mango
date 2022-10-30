998fd7ba90d9b3e88f3306a344acbb953e612e05
Daniel Molnar <daniel4molnar@gmail.com>
2022-10-30 17:46:28 +0200

    Extract icon component

--- lib/sase_mango_web/live/shared_components/header_component.ex
+++ lib/sase_mango_web/live/shared_components/header_component.ex
@@ -6,16 +6,7 @@ defmodule SaseMangoWeb.SharedComponents.HeaderComponent do
     <div class="mt-[120px] mb-[2rem] text-[17px] 2xl:text-[1.2em] py-4">
       <div class="flex items-center gap-4 px-2">
         <span class="text-[#8C8C8C] font-[400]">Date:</span>
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M11.6667 2.66663H2.33333C1.59695 2.66663 1 3.26358 1 3.99996V13.3333C1 14.0697 1.59695 14.6666 2.33333 14.6666H11.6667C12.403 14.6666 13 14.0697 13 13.3333V3.99996C13 3.26358 12.403 2.66663 11.6667 2.66663Z"
-              stroke="#5B92D7"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-          />
-          <path d="M9.66663 1.33337V4.00004" stroke="#5B92D7" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M4.33337 1.33337V4.00004" stroke="#5B92D7" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M1 6.66663H13" stroke="#5B92D7" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+        <.date_icon />
 
         <span class="font-[400]"><%= todays_date() %></span>
       </div>
@@ -44,6 +35,21 @@ defmodule SaseMangoWeb.SharedComponents.HeaderComponent do
 
       </div>
     </div>
+    """
+  end
+
+  def date_icon(assigns) do
+    ~H"""
+      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M11.6667 2.66663H2.33333C1.59695 2.66663 1 3.26358 1 3.99996V13.3333C1 14.0697 1.59695 14.6666 2.33333 14.6666H11.6667C12.403 14.6666 13 14.0697 13 13.3333V3.99996C13 3.26358 12.403 2.66663 11.6667 2.66663Z"
+            stroke="#5B92D7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path d="M9.66663 1.33337V4.00004" stroke="#5B92D7" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M4.33337 1.33337V4.00004" stroke="#5B92D7" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M1 6.66663H13" stroke="#5B92D7" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
     """
   end
 
