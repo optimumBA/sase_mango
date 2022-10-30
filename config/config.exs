15668363d63f941703e55f474bbdf33266644d5b
Daniel Molnar <daniel4molnar@gmail.com>
2022-10-30 21:30:22 +0200

    Run securities cache modules at startup

--- config/config.exs
+++ config/config.exs
@@ -46,7 +46,10 @@ config :sase_mango, SaseMango.Scheduler,
     {"*/10 9 * * 1-5", {SaseMango.TickerUpdater, :update, []}},
     # Every 5 minutes while markets are open (Mon-Fri 10:00-13:30)
     {"*/5 10-12 * * 1-5", {SaseMango.TickerUpdater, :update, []}},
-    {"0-30/5 13 * * 1-5", {SaseMango.TickerUpdater, :update, []}}
+    {"0-30/5 13 * * 1-5", {SaseMango.TickerUpdater, :update, []}},
+    # Run SecuritiesCache GenServer at startup
+    {"@reboot", {SaseMango.SecuritiesCache, :update_securities, []}},
+    {"@reboot", {SaseMango.BargainsCache, :update_bargains, []}}
   ],
   timezone: "Europe/Sarajevo"
 
