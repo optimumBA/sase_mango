f3c5abf15f7b107132716d1c396f1637851f043c
Daniel Molnar <daniel4molnar@gmail.com>
2022-10-30 17:41:08 +0200

    Some minor fixes

--- lib/sase_mango_web/live/shared_components/custom_select_component.ex
+++ lib/sase_mango_web/live/shared_components/custom_select_component.ex
@@ -31,16 +31,16 @@ defmodule SaseMangoWeb.SharedComponents.CustomSelectComponent do
       _other ->
         filtered_options =
           Enum.filter(options_from_socket, fn opt ->
-            String.starts_with?(opt.key, value) || String.starts_with?(opt.value, value)
+            String.starts_with?(opt.symbol, value) || String.starts_with?(opt.name, value)
           end)
 
         suggested_element =
           if length(filtered_options) > 0 do
             f_element = List.first(filtered_options)
 
-            is_suggested_by_symbol? = String.starts_with?(f_element.key, value)
+            is_suggested_by_symbol? = String.starts_with?(f_element.symbol, value)
 
-            if is_suggested_by_symbol?, do: f_element.key, else: f_element.value
+            if is_suggested_by_symbol?, do: f_element.symbol, else: f_element.name
           end
 
         if String.length(value) > 0, do: send(self(), {:update_state})
@@ -70,40 +70,38 @@ defmodule SaseMangoWeb.SharedComponents.CustomSelectComponent do
           phx-click="toggle"
         >
             <%= if @input_flip do %>
-              <div id="input-cover" class="absolute z-[10] bg-white flex items-center gap-2 text-left py-[0.9rem] mr-16"
-              phx-hook="FieldReset"
-
+              <div id="input-cover" class=" z-[10] bg-white flex items-center gap-2 text-left py-[0.9rem] mr-16"
+                phx-hook="FieldReset"
               >
-                <span class="block truncate text-[#5B92D7]"><%= @selected_item.key %></span>
-                <span class="block truncate text-[#1E1E1E]"><%= @selected_item.value %></span>
+                <span class="block truncate text-[#5B92D7]"><%= @selected_item.symbol %></span>
+                <span class="block truncate text-[#1E1E1E]"><%= @selected_item.name %></span>
               </div>
             <% end %>
-            <div class="relative">
+            <div class={"#{if !@input_flip, do: "relative", else: ""}"}>
               <input
                 autocomplete="off"
                 id="select-field"
                 type="text"
                 placeholder="SELECT"
-                class="border-0 p-[1rem] tracking-normal bg-transparent text-[1.2em] font-[300] pr-8 placeholder:focus:text-transparent"
+                class={"#{if @input_flip, do: "absolute top-1 left-1 -z-[10]", else: "block"} border-0 p-[1rem] tracking-normal bg-transparent text-[1.2em] font-[300] pr-8 placeholder:focus:text-transparent"}
                 phx-keyup="select_input_changed"
-
                 phx-target={@myself}
               />
-              </div>
+            </div>
 
-              <span class={"absolute #{if @suggested_element, do: "opacity-60", else: "opacity-0" } top-1/2 -translate-y-1/2 pl-[1rem] tracking-normal text-[1.2em] font-[300] text-[#a3a3a3]"} >
-                <%= @suggested_element %>
-              </span>
+            <span class={"absolute #{if @suggested_element, do: "opacity-60", else: "opacity-0" } top-1/2 -translate-y-1/2 pl-[1rem] tracking-normal text-[1.2em] font-[300] text-[#a3a3a3]"} >
+              <%= @suggested_element %>
+            </span>
 
-          <span class="absolute inset-y-0 right-0 flex items-center pr-4 ml-3">
-            <svg width="15" height="8"
-              viewBox="0 0 15 9"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M1 1L7.08696 7L13 1" stroke="#9A9A9A" stroke-width="2" stroke-linecap="round"/>
-            </svg>
-          </span>
+            <span class="absolute inset-y-0 right-0 flex items-center pr-4 ml-3">
+              <svg width="15" height="8"
+                viewBox="0 0 15 9"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M1 1L7.08696 7L13 1" stroke="#9A9A9A" stroke-width="2" stroke-linecap="round"/>
+              </svg>
+            </span>
         </div>
         <%= if @open do %>
           <ul
@@ -117,13 +115,13 @@ defmodule SaseMangoWeb.SharedComponents.CustomSelectComponent do
               >
                 <div class="w-full flex flex-col gap-2 py-2 pl-3 pr-9 border-b border-gray-100 cursor-pointer hover:bg-sky-100"
                   phx-click="custom_select"
-                  phx-value-symbol={option.key}
+                  phx-value-symbol={option.symbol}
                 >
                   <span class="block ml-3 font-normal text-[#5B92D7]">
-                    <%= option.key %>
+                    <%= option.symbol %>
                   </span>
                   <span class="w-max block ml-3 font-normal text-[#1E1E1E]">
-                    <%= option.value %>
+                    <%= option.name %>
                   </span>
                 </div>
               </li>
