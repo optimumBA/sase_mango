16372751d72a2524a01c6db94a1824cb723b319d
Daniel Molnar <daniel4molnar@gmail.com>
2022-10-30 17:40:04 +0200

    Add form input_wrapper component

--- lib/sase_mango_web/live/shared_components/form_components.ex
+++ lib/sase_mango_web/live/shared_components/form_components.ex
@@ -5,6 +5,47 @@ defmodule SaseMangoWeb.SharedComponents.FormComponents do
 
   use SaseMangoWeb, :component
 
+  def input_wrapper(assigns) do
+    assigns =
+      assigns
+      |> assign_new(:class, fn -> "" end)
+      |> assign_new(:error_tag, fn -> false end)
+      |> assign_new(:phx_feedback_for, fn -> input_name(assigns.form, assigns.field) end)
+      |> assign_new(:required, fn -> false end)
+      |> assign_new(:show_feedback, fn -> false end)
+
+    ~H"""
+    <div class={@class}
+      id={input_id(@form, @field) <> "_wrapper"}
+      phx-feedback-for={@phx_feedback_for}>
+      <%= label @form, @field, class: if(@show_feedback,
+          do: label_class(@form, @field),
+          else: "mb-[0.5rem] text-[#9A9A9A]")
+      do %>
+        <%= @label %>
+      <% end %>
+
+      <%= render_slot(@inner_block) %>
+
+      <%= if @error_tag do %>
+        <%= error_tag @form, @field %>
+      <% end %>
+    </div>
+    """
+  end
+
+  def label_class(form, field),
+    do:
+      if(field_has_error?(form, field),
+        do: "mb-[0.5rem] text-[#B9121C] label-invalid",
+        else: "mb-[0.5rem] text-[#9A9A9A]"
+      )
+
+  @spec field_has_error?(atom | %{:errors => keyword, optional(any) => any}, atom) :: boolean
+  def field_has_error?(form, field) do
+    Keyword.has_key?(form.errors, field)
+  end
+
   def submit_button(%{changeset: %Ecto.Changeset{valid?: valid?}, class: class} = assigns) do
     attrs =
       if valid? do
