83a541856d70fdab4fc520f98ef9b9d0e9dd59e8
senaid123 <senaid968@gmail.com>
2022-11-30 18:38:35 +0100

    add plug_cowboy to fix errors

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule SaseMango.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
+      {:plug_cowboy, "~> 2.6"},
       {:csv, "~> 2.4"},
       {:elixir_xml_to_map, "~> 2.0"},
       {:finch, "~> 0.9"},
