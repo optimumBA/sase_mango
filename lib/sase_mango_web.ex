5aac64560c5283d89a9070dfbfa43007d5127cd0
senaid123 <senaid968@gmail.com>
2022-11-30 18:41:04 +0100

    update LiveView imports

--- lib/sase_mango_web.ex
+++ lib/sase_mango_web.ex
@@ -45,7 +45,7 @@ defmodule SaseMangoWeb do
   def live_view do
     quote do
       use Phoenix.LiveView,
-        layout: {SaseMangoWeb.LayoutView, "live.html"}
+        layout: {SaseMangoWeb.LayoutView, :live}
 
       unquote(view_helpers())
     end
@@ -90,7 +90,7 @@ defmodule SaseMangoWeb do
       use Phoenix.HTML
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
-      import Phoenix.LiveView.Helpers
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
