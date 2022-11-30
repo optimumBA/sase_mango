d6189fa1bb17fad49a14ddeaceaf248c5e0771d1
senaid123 <senaid968@gmail.com>
2022-11-30 18:04:13 +0100

    update formatter and add phoenix_view 2.0 to deps

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule SaseMango.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.7.0-rc.0", override: true},
+      {:phoenix_view, "~> 2.0"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
