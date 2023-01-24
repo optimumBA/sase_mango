8f5f8a1e5d0d563a3345699e031214d07f0c81f1
Daniel Molnar <daniel4molnar@gmail.com>
2023-01-24 15:12:59 +0100

    Rename component & refactor code

--- lib/sase_mango_web/live/shared_components/issuer_select_component.ex
+++ lib/sase_mango_web/live/shared_components/issuer_select_component.ex
@@ -1,21 +1,20 @@
-defmodule SaseMangoWeb.SharedComponents.CustomSelectComponent do
+defmodule SaseMangoWeb.SharedComponents.IssuerSelectComponent do
   @moduledoc """
   Custom component that selects issuer smbol/name
   """
 
   use SaseMangoWeb, :live_component
 
+  alias Phoenix.LiveView.JS
+  alias SaseMangoWeb.SharedComponents.TableIconsComponent
+
   @impl true
   def update(assigns, socket) do
-    %{options: options} = assigns
-
-    socket =
-      socket
-      |> assign(:suggestions, options)
-      |> assign(:suggested_element, "")
-      |> assign(assigns)
-
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign(:suggestions, assigns.options)
+     |> assign(:suggested_element, nil)
+     |> assign(assigns)}
   end
 
   @impl true
@@ -26,11 +25,11 @@ defmodule SaseMangoWeb.SharedComponents.CustomSelectComponent do
 
     case key do
       "Enter" ->
-        if String.length(value) > 0, do: send(self(), {:update_state, value})
+        if String.length(search_value) > 0, do: send(self(), {:update_state, search_value})
 
         {:noreply, socket}
 
-      _other ->
+      _value ->
         filtered_options =
           Enum.filter(options_from_socket, fn option ->
             String.starts_with?(String.downcase(option.symbol), search_value) ||
@@ -67,27 +66,26 @@ defmodule SaseMangoWeb.SharedComponents.CustomSelectComponent do
   def render(assigns) do
     ~H"""
     <div id={@id}>
-      <div class="relative mt-1">
+      <div class="relative mt-1" phx-click-away={JS.hide(to: "#issuers-list")}>
         <div
-          class="relative cursor-pointer w-full sm:min-w-[380px] w-full px-[1rem] py-0 text-left bg-white border border-[#979797] hover:border-[#565555] rounded-[0.8rem] shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 font-[300]"
-          phx-click-away="hide_select"
-          phx-click="toggle_select"
+          class="relative cursor-pointer w-full sm:min-w-[380px] px-[1rem] py-0 text-left bg-white border border-[#979797] hover:border-[#565555] rounded-[0.8rem] shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 font-[300]"
+          phx-click={JS.toggle(to: "#issuers-list")}
         >
-            <%= if @input_flip do %>
+            <%= if @issuer_input_cover do %>
               <div id="input-cover" class="z-[10] text-[0.86em] xl:text-[1.02em] bg-white flex items-center gap-2 text-left py-[0.9rem] mr-16"
                 phx-hook="FieldReset"
               >
-                <span class="block truncate text-[#5B92D7]"><%= @selected_item.symbol %></span>
-                <span class="block truncate text-[#1E1E1E]"><%= @selected_item.name %></span>
+                <span class="block truncate text-[#5B92D7]"><%= @selected_issuer.symbol %></span>
+                <span class="block truncate text-[#1E1E1E]"><%= @selected_issuer.name %></span>
               </div>
             <% end %>
-            <div class={if(@input_flip, do: "", else: "relative")}>
+            <div class={if(@issuer_input_cover, do: "", else: "relative")}>
               <input
                 autocomplete="off"
                 id="select-field"
                 type="text"
                 placeholder="SELECT"
-                class={if(@input_flip, do: "absolute top-1 left-1 -z-[10]", else: "block") <> " border-0 p-[1rem] tracking-normal bg-transparent text-[1em] xl:text-[1.2em] font-[300] pr-8 placeholder:focus:text-transparent"}
+                class={if(@issuer_input_cover, do: "absolute top-1 left-1 -z-[10]", else: "block") <> " border-0 p-[1rem] tracking-normal bg-transparent text-[1em] xl:text-[1.2em] font-[300] pr-8 placeholder:focus:text-transparent"}
                 phx-keyup="select_input_changed"
                 phx-target={@myself}
               />
@@ -98,40 +96,31 @@ defmodule SaseMangoWeb.SharedComponents.CustomSelectComponent do
             </span>
 
             <span class="absolute inset-y-0 right-0 flex items-center pr-4 ml-3">
-              <svg width="15" height="8"
-                viewBox="0 0 15 9"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M1 1L7.08696 7L13 1" stroke="#9A9A9A" stroke-width="2" stroke-linecap="round"/>
-              </svg>
+              <TableIconsComponent.arrow_down />
             </span>
         </div>
-        <%= if @open do %>
-          <ul
-              class="absolute z-[10] sm:min-w-[380px] py-1 mt-2 overflow-y-auto text-[1em] font-[300] max-h-[34rem] bg-white drop-shadow-lg rounded-lg ring-2 ring-black ring-opacity-5 focus:outline-none"
-              role="selectable-options"
-            >
-            <%= for option <- @suggestions do %>
+          <ul id="issuers-list"
+            class="absolute hidden z-[10] w-full sm:min-w-[380px] py-1 mt-2 overflow-y-auto text-[1em] font-[300] max-h-[34rem] bg-white shadow-lg rounded-lg ring-1 ring-gray-400 ring-opacity-25 focus:outline-none"
+            role="selectable-options"
+          >
+            <%= for suggestion <- @suggestions do %>
               <li
                 class="relative select-none"
                 role="option"
+                phx-click={JS.push("select_issuer", value: %{symbol: suggestion.symbol}) |> JS.hide(to: "#issuers-list")}
               >
                 <div class="w-full flex flex-col gap-2 py-2 pl-3 pr-9 border-b border-gray-100 cursor-pointer hover:bg-sky-100"
-                  phx-click="select_item"
-                  phx-value-symbol={option.symbol}
                 >
                   <span class="block ml-3 font-normal text-[#5B92D7]">
-                    <%= option.symbol %>
+                    <%= suggestion.symbol %>
                   </span>
                   <span class="w-max block ml-3 font-normal text-[#1E1E1E]">
-                    <%= option.name %>
+                    <%= suggestion.name %>
                   </span>
                 </div>
               </li>
             <% end %>
           </ul>
-        <% end %>
       </div>
     </div>
     """
