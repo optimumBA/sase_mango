230ca61caea91af021c628df6b1087e64659458e
Daniel Molnar <daniel4molnar@gmail.com>
2023-02-01 20:43:12 +0100

    Put more logic inside table cell components

--- lib/sase_mango_web/live/securities_live/table_components.ex
+++ lib/sase_mango_web/live/securities_live/table_components.ex
@@ -50,22 +50,58 @@ defmodule SaseMangoWeb.SecuritiesLive.TableComponents do
   end
 
   def one_value_number_cell(assigns) do
+    maybe_add_percentage = maybe_add_percentage(assigns[:percentage])
+
+    format_value =
+      assigns.value
+      |> maybe_round_value(assigns[:round])
+      |> maybe_format_with_delimiter(assigns[:delimiter], assigns[:separator])
+
+    assigns = assign(assigns, :value, format_value)
+
     ~H"""
-    <td class="text-right 2xl:text-xl p-2 2xl:px-4 2xl:py-2"><%= @value %></td>
+    <td class="text-right 2xl:text-xl p-2 2xl:px-4 2xl:py-2"><%= @value %><%= maybe_add_percentage %></td>
     """
   end
 
   def double_value_number_cell(assigns) do
-    special_sign = if(assigns[:procent], do: "%", else: "")
+    maybe_add_percentage = maybe_add_percentage(assigns[:percentage])
+
+    format_first_value =
+      assigns.first_value
+      |> maybe_round_value(assigns[:round_first])
+      |> maybe_format_with_delimiter(assigns[:delimiter], assigns[:separator])
+
+    format_second_value =
+      assigns.second_value
+      |> maybe_round_value(assigns[:round_second])
+      |> maybe_format_with_delimiter(assigns[:delimiter], assigns[:separator])
+
+    assigns =
+      assigns
+      |> assign(:first_value, format_first_value)
+      |> assign(:second_value, format_second_value)
 
     ~H"""
     <td class="p-2 2xl:px-4 2xl:py-2">
       <div class="flex flex-col gap-4 text-right">
-          <span><%= @first_value %><%= special_sign %></span>
-          <span><%= @second_value %><%= special_sign %></span>
+          <span><%= @first_value %><%= maybe_add_percentage %></span>
+          <span><%= @second_value %><%= maybe_add_percentage %></span>
       </div>
     </td>
     """
+  end
+
+  defp maybe_round_value(value, nil), do: value
+  defp maybe_round_value(value, round), do: Decimal.round(value, round)
+
+  defp maybe_add_percentage(nil), do: ""
+  defp maybe_add_percentage(_percentage), do: "%"
+
+  defp maybe_format_with_delimiter(value, nil, nil), do: value
+
+  defp maybe_format_with_delimiter(value, delimiter, separator) do
+    Number.Delimit.number_to_delimited(value, delimiter: delimiter, separator: separator)
   end
 
   defp get_icon_color(%{sort_by: sort_key} = sort_options, key) when sort_key == key do
@@ -74,8 +110,8 @@ defmodule SaseMangoWeb.SecuritiesLive.TableComponents do
 
   defp get_icon_color(_sort_options, _key), do: nil
 
-  defp icon_color(:asc), do: "text-sort-asc"
-  defp icon_color(:desc), do: "text-sort-desc"
+  defp icon_color(:asc), do: "text-green-350"
+  defp icon_color(:desc), do: "text-red-550"
 
   defp get_icon_direction(:number, %{sort_by: sort_key} = sort_options, key)
        when sort_key == key do
