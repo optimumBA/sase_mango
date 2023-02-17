34444f30ab4679838cff15938573a397bff73d1f
Daniel Molnar <daniel4molnar@gmail.com>
2023-02-17 18:55:50 +0100

    Add helper hook to select field

--- assets/js/hooks/calculator_input_hooks.js
+++ assets/js/hooks/calculator_input_hooks.js
@@ -7,12 +7,31 @@ CalculatorInputHooks.FieldReset = {
         let inputElement = document.getElementById('select-field')
 
         flipElement.addEventListener('click', (e) => {
-
             this.pushEvent('toggle_issuer_input_cover', {})
-
             inputElement.focus()
             inputElement.value = ''
+        })
+    }
+}
 
+CalculatorInputHooks.InputField = {
+    mounted() {
+        const hook = this
+        const inputElement = hook.el
+
+        inputElement.addEventListener('keyup', (event) => {
+            if (event.code == 'ArrowUp' || event.code == 'ArrowDown') {
+                hook.pushEventTo('#issuer-select-comp', 'scroll_list', { key: event.code })
+            } else if (event.code != 'Tab' && event.code != 'Enter') {
+                hook.pushEventTo('#issuer-select-comp', 'select_input_changed', { key: event.code, value: event.target.value })
+            }
+        })
+
+        inputElement.addEventListener('keydown', (event) => {
+            if (event.code == 'Tab' || event.code == 'Enter') {
+                inputElement.blur()
+                hook.pushEventTo('#issuer-select-comp', 'select_input_changed', { key: event.code, value: event.target.value })
+            }
         })
     }
 }
