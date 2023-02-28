9eefc2bf935a97d242277ee26b77fcbe9d499bdb
Daniel Molnar <daniel4molnar@gmail.com>
2023-02-28 13:07:12 +0100

    Make fields nullable

--- priv/repo/migrations/20230225124519_add_company_data_for_issuers.exs
+++ priv/repo/migrations/20230225124519_add_company_data_for_issuers.exs
@@ -3,8 +3,8 @@ defmodule SaseMango.Repo.Migrations.AddCompanyDataForIssuers do
 
   def change do
     alter table(:issuers) do
-      add :company_data, :map, null: false
-      add :top_10_owners, :map, null: false
+      add :company_data, :map
+      add :top_10_owners, :map
     end
   end
 end
