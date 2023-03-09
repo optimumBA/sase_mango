8d53779f8bf2379ce6cc9b43c61e45cc51379944
Daniel Molnar <daniel4molnar@gmail.com>
2023-03-09 10:00:21 +0100

    Company info issuer link (#11)

--- lib/sase_mango_web/live/securities_live/show.ex
+++ lib/sase_mango_web/live/securities_live/show.ex
@@ -45,17 +45,15 @@ defmodule SaseMangoWeb.SecuritiesLive.Show do
   def render(assigns) do
     ~H"""
       <section class="w-full min-h-screen">
-        <header class="p-4 border-b shadow-md mb-4">
-          <div class="w-full md:w-11/12 xl:w-3/4 mx-auto px-2 md:px-0 py-2 mt-8">
+        <header class="py-4 px-4 md:px-0 border-b shadow-md mb-4">
+          <div class="w-full md:w-11/12 xl:w-3/4 mx-auto px-2 md:px-0 py-2 mt-10">
             <div class="relative ml-6 md:ml-0">
-              <h2 class="text-blue-dark-200 text-lg font-semibold md:text-2xl mb-2 tracking-wide">
-                <%= @company_data.name %>
-              </h2>
+                <TableIconsComponent.issuer_link company_symbol={@company_data.symbol} company_name={@company_data.name} />
               <%= live_redirect to: Routes.securities_index_path(@socket, :securities), class: "absolute top-1/2 left-0 -translate-x-8 -translate-y-1/2" do %>
                 <TableIconsComponent.icon_back />
               <% end %>
             </div>
-            <p class="font-light text-gray-450 text-sm ml-6 md:ml-0">Company information</p>
+            <p class="font-light text-gray-450 text-sm ml-6 mt-2 md:ml-0">Company information</p>
           </div>
         </header>
 
