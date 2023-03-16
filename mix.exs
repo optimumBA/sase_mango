30e86fe81832887721434ecb5e4293c80ea0ed7d
kagure-nyakio <kagurenyakio@gmail.com>
2023-03-16 10:45:33 +0300

    Check if any steps for upgrade have been skipped

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule SaseMango.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0-rc.0", override: true},
+      {:phoenix, "~> 1.7.0"},
       {:phoenix_view, "~> 2.0"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
