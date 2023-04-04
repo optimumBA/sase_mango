dd937106c708ec7e49c8b7c8d5085741b43d737b
Daniel Molnar <daniel4molnar@gmail.com>
2023-04-04 15:05:54 +0200

    Add component documentation, declare attributes

--- lib/sase_mango_web/live/securities_live/table_components.ex
+++ lib/sase_mango_web/live/securities_live/table_components.ex
@@ -7,6 +7,20 @@ defmodule SaseMangoWeb.SecuritiesLive.TableComponents do
 
   alias SaseMangoWeb.SharedComponents.TableIconsComponent
 
+  @doc """
+  Shows the table sort component with column title and sort icon.
+
+  ## Examples
+
+      <th :for={table_column <- @table_columns}>
+          ...
+            <.sort_link column={table_column} sort_options={@sort_options} />
+          ...
+      </th>
+  """
+  attr :column, :map, required: true
+  attr :sort_options, :map, required: true
+
   def sort_link(assigns) do
     ~H"""
     <div
@@ -23,11 +37,29 @@ defmodule SaseMangoWeb.SecuritiesLive.TableComponents do
           col_type={@column.type}
           sort_options={@sort_options}
           key={@column.name}
-         />
+        />
       </div>
     </div>
     """
   end
+
+  @doc """
+  Renders the table row for securities with content as a slot.
+
+  ## Examples
+
+      <%= for security <- @securities do %>
+        <.table_row security={security} >
+            <td class="...">
+              ...
+            </td>
+            ...
+        </.table_row>
+      <% end %>
+  """
+  attr :class, :string, default: nil
+  slot :inner_block, required: true
+  attr :security, :map, required: true
 
   def table_row(%{security: security} = assigns) do
     class =
@@ -51,6 +83,20 @@ defmodule SaseMangoWeb.SecuritiesLive.TableComponents do
     """
   end
 
+  @doc """
+  Renders the table number cell component for one value.
+
+  ## Examples
+
+      <.one_value_number_cell class="text-right ..." value={price} round={2} />
+  """
+  attr :class, :string
+  attr :delimiter, :string
+  attr :percentage, :boolean, default: false
+  attr :round, :integer
+  attr :separator, :string
+  attr :value, :float, required: true
+
   def one_value_number_cell(assigns) do
     maybe_add_percentage = maybe_add_percentage(assigns[:percentage])
 
@@ -59,12 +105,32 @@ defmodule SaseMangoWeb.SecuritiesLive.TableComponents do
       |> maybe_round_value(assigns[:round])
       |> maybe_format_with_delimiter(assigns[:delimiter], assigns[:separator])
 
-    assigns = assign(assigns, value: format_value, maybe_add_percentage: maybe_add_percentage)
+    assigns =
+      assigns
+      |> assign(:value, format_value)
+      |> assign(:maybe_add_percentage, maybe_add_percentage)
+      |> assign_new(:class, fn -> "text-center" end)
 
     ~H"""
-    <td class="text-right 2xl:text-xl p-2 2xl:px-4 2xl:py-2"><%= @value %><%= @maybe_add_percentage %></td>
+    <td class={@class}><%= @value %><%= @maybe_add_percentage %></td>
     """
   end
+
+  @doc """
+  Renders the table number cell component for two values.
+
+  ## Examples
+
+      <.double_value_number_cell class="text-right ..." first_value={ask_price} second_value={ask_volume} round_first={2}/>
+  """
+  attr :class, :string
+  attr :delimiter, :string
+  attr :first_value, :float, required: true
+  attr :percentage, :boolean, default: false
+  attr :round_first, :integer
+  attr :round_second, :integer
+  attr :second_value, :float, required: true
+  attr :separator, :string
 
   def double_value_number_cell(assigns) do
     maybe_add_percentage = maybe_add_percentage(assigns[:percentage])
@@ -84,12 +150,13 @@ defmodule SaseMangoWeb.SecuritiesLive.TableComponents do
       |> assign(:maybe_add_percentage, maybe_add_percentage)
       |> assign(:first_value, format_first_value)
       |> assign(:second_value, format_second_value)
+      |> assign_new(:class, fn -> "text-center" end)
 
     ~H"""
     <td class="p-2 2xl:px-4 2xl:py-2">
-      <div class="flex flex-col gap-4 text-right">
-          <span><%= @first_value %><%= @maybe_add_percentage %></span>
-          <span><%= @second_value %><%= @maybe_add_percentage %></span>
+      <div class={@class}>
+        <span><%= @first_value %><%= @maybe_add_percentage %></span>
+        <span><%= @second_value %><%= @maybe_add_percentage %></span>
       </div>
     </td>
     """
@@ -98,8 +165,8 @@ defmodule SaseMangoWeb.SecuritiesLive.TableComponents do
   defp maybe_round_value(value, nil), do: value
   defp maybe_round_value(value, round), do: Decimal.round(value, round)
 
-  defp maybe_add_percentage(nil), do: ""
-  defp maybe_add_percentage(_percentage), do: "%"
+  defp maybe_add_percentage(false), do: ""
+  defp maybe_add_percentage(true), do: "%"
 
   defp maybe_format_with_delimiter(value, nil, nil), do: value
 
