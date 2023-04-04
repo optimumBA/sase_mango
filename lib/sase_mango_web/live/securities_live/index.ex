81c85993bc211413b0ff44d6fa58a26f61c83d19
Daniel Molnar <daniel4molnar@gmail.com>
2023-04-04 15:39:42 +0200

    Reuse some code from helper module

--- lib/sase_mango_web/live/securities_live/index.ex
+++ lib/sase_mango_web/live/securities_live/index.ex
@@ -35,7 +35,7 @@ defmodule SaseMangoWeb.SecuritiesLive.Index do
 
   defp assign_sort_options(socket, params) do
     new_sort_by = params["sort_by"] || "eps_roi"
-    new_sort_order = set_sort_order(params["sort_order"])
+    new_sort_order = HandleTable.set_sort_order(params["sort_order"])
     sort_options = %{sort_by: new_sort_by, sort_order: new_sort_order}
 
     assign(socket, :sort_options, sort_options)
@@ -90,7 +90,7 @@ defmodule SaseMangoWeb.SecuritiesLive.Index do
   end
 
   def handle_info({:url_update, options}, socket) do
-    url_params = merge_url_params(socket, options)
+    url_params = HandleTable.merge_url_params(socket, options)
     path = Routes.securities_index_path(socket, socket.assigns.live_action, url_params)
 
     {:noreply, push_patch(socket, to: path, replace: true)}
@@ -116,36 +116,22 @@ defmodule SaseMangoWeb.SecuritiesLive.Index do
       if col_name != name do
         :asc
       else
-        revert_sort_order(sort_order)
+        HandleTable.revert_sort_order(sort_order)
       end
 
     sort_options = %{sort_by: name, sort_order: maybe_update_sort_order}
 
-    url_params = merge_url_params(socket, sort_options)
+    url_params = HandleTable.merge_url_params(socket, sort_options)
     path = Routes.securities_index_path(socket, socket.assigns.live_action, url_params)
 
     {:noreply, push_patch(socket, to: path, replace: true)}
   end
 
   def handle_event("clear_form", _params, socket) do
-    url_params = merge_url_params(socket, %{q: nil})
+    url_params = HandleTable.merge_url_params(socket, %{q: nil})
     path = Routes.securities_index_path(socket, socket.assigns.live_action, url_params)
 
     {:noreply, push_patch(socket, to: path, replace: true)}
-  end
-
-  defp merge_url_params(socket, options) do
-    %{sort_options: sort_options, filter_options: filter_options} = socket.assigns
-
-    url_params =
-      %{}
-      |> Map.merge(sort_options)
-      |> Map.merge(Map.from_struct(filter_options))
-      |> Map.merge(options)
-      |> Enum.reject(fn {_key, value} -> is_nil(value) end)
-      |> Map.new()
-
-    url_params
   end
 
   defp maybe_filter_securities(%{assigns: %{filter_options: %{q: query}}} = socket)
@@ -173,11 +159,4 @@ defmodule SaseMangoWeb.SecuritiesLive.Index do
   end
 
   defp sort_securities(socket, _list), do: socket
-
-  defp set_sort_order("asc"), do: :asc
-  defp set_sort_order("desc"), do: :desc
-  defp set_sort_order(_value), do: :desc
-
-  defp revert_sort_order(:asc), do: :desc
-  defp revert_sort_order(:desc), do: :asc
 end
