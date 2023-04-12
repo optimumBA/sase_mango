d6a6ad0fe460cf341c7da98b52dfa036a0bef316
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-04-12 11:18:39 +0200

    Enable preview app deployment

--- .github/github_workflows.ex
+++ .github/github_workflows.ex
@@ -5,8 +5,13 @@ defmodule GitHubWorkflows do
   """
 
   def get do
+    repo_name = "sase_mango"
+    app_name = "sase-mango"
+
     %{
-      "main.yml" => main_workflow()
+      "main.yml" => main_workflow(),
+      "pr.yml" => pr_workflow(repo_name, app_name),
+      "pr_closure.yml" => pr_closure_workflow(repo_name, app_name)
     }
   end
 
@@ -20,15 +25,136 @@ defmodule GitHubWorkflows do
           ]
         ],
         jobs: [
-          deploy: deploy_job()
+          deploy_staging_app: deploy_staging_app_job()
         ]
       ]
     ]
   end
 
-  defp deploy_job do
+  defp pr_workflow(repo_name, app_name) do
     [
-      name: "Deploy to Fly.io",
+      [
+        name: "PR",
+        on: [
+          pull_request: [
+            branches: ["main"],
+            types: ["opened", "reopened", "synchronize"]
+          ]
+        ],
+        jobs: [
+          deploy_preview_app: deploy_preview_app_job(repo_name, app_name)
+        ]
+      ]
+    ]
+  end
+
+  defp pr_closure_workflow(repo_name, app_name) do
+    [
+      [
+        name: "PR closure",
+        on: [
+          pull_request: [
+            branches: ["main"],
+            types: ["closed"]
+          ]
+        ],
+        jobs: [
+          delete_preview_app: delete_preview_app_job(repo_name, app_name)
+        ]
+      ]
+    ]
+  end
+
+  defp delete_preview_app_job(repo_name, app_name) do
+    [
+      name: "Delete preview app",
+      "runs-on": "ubuntu-latest",
+      concurrency: [group: "pr-${{ github.event.number }}"],
+      env: [
+        FLY_API_TOKEN: "${{ secrets.FLY_API_TOKEN }}",
+        REPO_NAME: repo_name
+      ],
+      steps: [
+        [
+          uses: "actions/checkout@v2"
+        ],
+        [
+          name: "Delete preview app",
+          uses: "almirsarajcic/fly-pr-review-apps@remote-builder",
+          with: [
+            name: "pr-${{ github.event.number }}-#{app_name}"
+          ]
+        ],
+        [
+          name: "Generate token",
+          uses: "navikt/github-app-token-generator@v1.1.1",
+          id: "generate_token",
+          with: [
+            "app-id": "${{ secrets.GH_APP_ID }}",
+            "private-key": "${{ secrets.GH_APP_PRIVATE_KEY }}"
+          ]
+        ],
+        [
+          name: "Delete GitHub environment",
+          uses: "strumwolf/delete-deployment-environment@v2.2.3",
+          with: [
+            token: "${{ steps.generate_token.outputs.token  }}",
+            environment: "pr-${{ github.event.number }}-#{app_name}",
+            ref: "${{ github.head_ref }}"
+          ]
+        ]
+      ]
+    ]
+  end
+
+  defp deploy_preview_app_job(repo_name, app_name) do
+    [
+      name: "Deploy preview app",
+      permissions: "write-all",
+      "runs-on": "ubuntu-latest",
+      concurrency: [group: "pr-${{ github.event.number }}"],
+      env: [
+        FLY_API_TOKEN: "${{ secrets.FLY_API_TOKEN }}",
+        FLY_ORG: "optimum-bh",
+        FLY_REGION: "fra",
+        PHX_HOST: "pr-${{ github.event.number }}-#{app_name}.fly.dev",
+        REPO_NAME: repo_name
+      ],
+      environment: [
+        name: "pr-${{ github.event.number }}-#{app_name}",
+        url: "https://${{ env.PHX_HOST }}"
+      ],
+      steps: [
+        [
+          uses: "actions/checkout@v2"
+        ],
+        [
+          name: "Delete previous deployments",
+          uses: "strumwolf/delete-deployment-environment@v2.2.3",
+          with: [
+            token: "${{ secrets.GITHUB_TOKEN }}",
+            environment: "pr-${{ github.event.number }}-#{app_name}",
+            ref: "${{ github.head_ref }}",
+            onlyRemoveDeployments: true
+          ]
+        ],
+        [
+          name: "Deploy preview app",
+          uses: "almirsarajcic/fly-pr-review-apps@remote-builder",
+          with: [
+            name: "pr-${{ github.event.number }}-#{app_name}",
+            secrets:
+              "ADMIN_PASSWORD=${{ secrets.ADMIN_PASSWORD }} MAXMIND_LICENSE_KEY=${{ secrets.MAXMIND_LICENSE_KEY }} PHX_HOST=${{ env.PHX_HOST }} SECRET_KEY_BASE=${{ secrets.SECRET_KEY_BASE }}",
+            vm_memory: 1024
+          ]
+        ]
+      ]
+    ]
+  end
+
+  defp deploy_staging_app_job do
+    [
+      name: "Deploy staging app",
       "runs-on": "ubuntu-latest",
       env: [
         FLY_API_TOKEN: "${{ secrets.FLY_API_TOKEN }}"
