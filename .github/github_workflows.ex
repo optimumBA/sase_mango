ce70a863ad05c8e237ce671b48e0b5af7cb01472
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-04-13 10:08:18 +0200

    Enforce Nomad platform usage

--- .github/github_workflows.ex
+++ .github/github_workflows.ex
@@ -80,7 +80,7 @@ defmodule GitHubWorkflows do
         ],
         [
           name: "Delete preview app",
-          uses: "almirsarajcic/fly-pr-review-apps@machines",
+          uses: "almirsarajcic/fly-pr-review-apps@nomad",
           with: [
             name: "pr-${{ github.event.number }}-#{app_name}"
           ]
@@ -140,7 +140,7 @@ defmodule GitHubWorkflows do
         ],
         [
           name: "Deploy preview app",
-          uses: "almirsarajcic/fly-pr-review-apps@machines",
+          uses: "almirsarajcic/fly-pr-review-apps@nomad",
           with: [
             name: "pr-${{ github.event.number }}-#{app_name}",
             secrets:
