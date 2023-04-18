8c8124135a34bd2253fb9908891bde30bc29efa8
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-04-18 10:45:23 +0200

    Use our fork

--- .github/github_workflows.ex
+++ .github/github_workflows.ex
@@ -80,7 +80,7 @@ defmodule GitHubWorkflows do
         ],
         [
           name: "Delete preview app",
-          uses: "almirsarajcic/fly-pr-review-apps@region",
+          uses: "optimumBA/fly-preview-apps@main",
           with: [
             name: "pr-${{ github.event.number }}-#{app_name}"
           ]
@@ -140,7 +140,7 @@ defmodule GitHubWorkflows do
         ],
         [
           name: "Deploy preview app",
-          uses: "almirsarajcic/fly-pr-review-apps@region",
+          uses: "optimumBA/fly-preview-apps@main",
           with: [
             name: "pr-${{ github.event.number }}-#{app_name}",
             secrets:
